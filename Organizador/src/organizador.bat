7333c77d6f2316b4a6c7677853dc0f636c71c72c
FelipeMovio <felipelsmovio@gmail.com>
2026-04-26 12:05:48 -0300

    deixando de maneira que rode automatico no windows

---
+++ Organizador/src/organizador.bat
@@ -0,0 +1,7 @@
+@echo off
+cd /d "C:\Users\felip\OneDrive\Desktop\repositorio\Java\Java Automacao\OrganizadorDePasta\Organizador"
+start "" javaw -jar organizador.jar
+
+@REM @echo off
+@REM cd /d "C:\Users\felip\OneDrive\Desktop\repositorio\Java\Java Automacao\OrganizadorDePasta\Organizador"
+@REM java -jar organizador.jar >> log.txt 2>&1
